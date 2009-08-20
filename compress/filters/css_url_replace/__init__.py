9c6a4a9e3498e1db4150991b8048231694cb0adc
Ryan Kelly <ryan@rfk.id.au>
2009-08-20 17:19:02 +1000

    allow any iterable for COMPRESS_CSS_URL_REPLACE

--- compress/filters/css_url_replace/__init__.py
+++ compress/filters/css_url_replace/__init__.py
@@ -8,8 +8,12 @@ class CSSURLReplace(FilterBase):
 
     def filter_css(self, css):
         filtered_css = css
-        if type(REPLACE_LIST) == list:
-            for REPLACE in REPLACE_LIST:
+        try:
+            replacements = iter(REPLACE_LIST)
+        except TypeError:
+            pass
+        else:
+            for REPLACE in replacements:
                 if len(REPLACE) == 2:
                     filtered_css = filtered_css.replace(REPLACE[0], REPLACE[1])
                     if self.verbose:
